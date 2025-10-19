e2c46375fadf65de523a2c0d216f0e6c85ecebb8
kakurokan <leokakali7@gmail.com>
2025-10-19 18:47:03 +0100

    Sem addAt

--- src/aed/collections/FintList.java
+++ src/aed/collections/FintList.java
@@ -1,53 +1,69 @@
 package aed.collections;
 
 import java.util.Iterator;
+import java.util.function.Consumer;
 
-public class FintList {
+public class FintList implements Iterable<Integer> {
     private static final int INITIAL_CAPACITY = 10;
 
-    private intNode[] elements; //Array de elementos da lista
+    private IntNode[] elements; //Array de elementos da lista
     private int free_index; //Tamanho da lista de elementos vazios
     private int head; //Primeiro elemento da lista
     private int tail; //Ultimo elemento da lista
     private int capacity; //Tamanho total da lista
 
-    private static class intNode { //Classe
+    @Override
+    public Iterator<Integer> iterator() {
+        return new FintListIterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Integer> action) {
+        Iterable.super.forEach(action);
+    }
+
+    public class FintListIterator() implements Iterator<Integer> {
+
+        int nextNodeIndex;
+
+        public FintListIterator() {
+            this.nextNodeIndex = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return nextNodeIndex != -1;
+        }
+
+        @Override
+        public Integer next() {
+            IntNode result = elements[nextNodeIndex];
+            nextNodeIndex = result.next_index;
+            return result.value;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Iterator doesn’t support removal");
+        }
+    }
+
+    private static class IntNode { //Classe
         int value;
         int next_index;
         int prev_index;
 
-        intNode(int value, int next_index, int prev_index) {
+        IntNode(int value, int next_index, int prev_index) {
             this.value = value;
             this.next_index = next_index;
             this.prev_index = prev_index;
         }
     }
 
-    public Iterator<Integer> iterator() {
-        return new Iterator<Integer>() {
-            private int current = head;
-
-            @Override
-            public boolean hasNext() {
-                return current != -1;
-            }
-
-            @Override
-            public Integer next() {
-                if (!hasNext()) {
-                    throw new java.util.NoSuchElementException();
-                }
-                int valor = current;
-                current = elements[current].next_index;
-                System.out.println(elements[valor].value);
-                return valor;
-            }
-        };
-    }
 
     public FintList() {
         this.capacity = INITIAL_CAPACITY;
-        this.elements = new intNode[capacity];
+        this.elements = new IntNode[capacity];
         this.free_index = 0;
         this.tail = -1;
         this.head = -1;
@@ -60,24 +76,27 @@ public class FintList {
     boolean add(int item) {
         if (elements == null)
             return false;
-        if (tail == -1) {
+        if (head == -1 && tail == -1) {
             this.head = this.free_index;
             this.tail = this.free_index;
-            elements[this.free_index] = new intNode(item, -1, -1);
+
+            elements[this.free_index] = new IntNode(item, -1, -1);
             System.out.println("Esta é a " + free_index + " e adicionou" + item);
             free_index++;
         } else {
-            intNode newNode = new intNode(item, -1, tail);
+            IntNode newNode = new IntNode(item, -1, tail);
 
-            int next_free_index = -1;
-            if (elements[tail].next_index != -1)
-                next_free_index = elements[tail].next_index;
+            int next_free_index = free_index;
+            if (elements[free_index] != null)
+                next_free_index = elements[free_index].next_index;
 
             elements[free_index] = newNode;
             elements[tail].next_index = free_index;
             tail = free_index;
+
             System.out.println("Esta é a " + free_index + " e adicionou" + newNode.value);
-            if (next_free_index != -1)
+
+            if (next_free_index != free_index)
                 free_index = next_free_index;
             else
                 free_index++;
@@ -89,12 +108,15 @@ public class FintList {
     int remove() {
         if (head == -1)
             throw new IndexOutOfBoundsException("Lista vazia");
-        intNode node = elements[tail]; //Guarda o no para ‘posteriori’
-        if (this.head == this.tail) {
-            this.free_index = 0;
-            this.tail = -1;
-            this.head = -1;
-            return node.value;
+        IntNode node = elements[tail]; //Guarda o no para ‘posteriori’
+        if (node.prev_index == -1) {
+            //Lógica do free_index para subtituição no add
+            elements[tail].next_index = free_index;
+            free_index = tail;
+            //---
+
+            head = -1;
+            tail = -1;
         } else {
             elements[elements[tail].prev_index].next_index = -1; //O no que apontava para a cauda para de apontar para qualquer no
 
@@ -104,13 +126,11 @@ public class FintList {
             //---
 
             tail = node.prev_index; //Muda a cauda para o no anterior
-
-            return node.value;
         }
-
+        return node.value;
     }
 
-    void addAt(int index, int item) {
+    /*void addAt(int index, int item) {
         if (index < 0) {
             throw new IndexOutOfBoundsException("Indice invalido");
         }
@@ -125,7 +145,7 @@ public class FintList {
         elements[elements[atual].prev_index].next_index = tail;
         elements[atual].prev_index = tail;
         tail = tail_temp;
-    }
+    }*/
 
     public static void main(String[] args) {
         FintList teste = new FintList();
@@ -135,7 +155,6 @@ public class FintList {
         teste.add(4);
         teste.add(5);
         teste.add(10);
-        teste.addAt(4,21);
         System.out.println(teste.remove());
         System.out.println(teste.remove());
         System.out.println(teste.remove());
