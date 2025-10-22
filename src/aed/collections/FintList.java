caf35668e2bd706157f5cff64b0cf3f12ace318f
kakurokan <leokakali7@gmail.com>
2025-10-22 08:50:05 +0100

    opcao com map

--- src/aed/collections/FintList.java
+++ src/aed/collections/FintList.java
@@ -36,7 +36,7 @@ public class FintList implements Iterable<Integer> {
         String[] partes;
         FintList teste_deep = new FintList();
         int a, b;
-        BinaryOperator<Integer> op_soma = (x, y) -> x + y;
+        BinaryOperator<Integer> op_soma = Integer::sum;
         BinaryOperator<Integer> op_mult = (x, y) -> x * y;
         BinaryOperator<Integer> op_sub = (x, y) -> x - y;
         BinaryOperator<Integer> op_div = (x, y) -> x / y;
@@ -60,7 +60,8 @@ public class FintList implements Iterable<Integer> {
         System.out.println("soma - Sum all elements");
         System.out.println("mult - Multiply all elements");
         System.out.println("div - Divide all elements");
-        System.out.println("sub - Subtract all elements\n");
+        System.out.println("sub - Subtract all elements");
+        System.out.println("map - Square all elements in the list\n");
 
         while ((linha = br.readLine()) != null) {
             partes = linha.split(" ");
@@ -92,7 +93,7 @@ public class FintList implements Iterable<Integer> {
                 System.out.println(teste.remove(a));
             } else if (linha.contains("removeAt")) {
                 a = Integer.parseInt(partes[1]);
-                teste.removeAt(a);
+                System.out.println(teste.removeAt(a));
             } else if (linha.contains("contains")) {
                 a = Integer.parseInt(partes[1]);
                 System.out.println(teste.contains(a));
@@ -114,6 +115,8 @@ public class FintList implements Iterable<Integer> {
                 System.out.println(teste.reduce(op_div, 1));
             } else if (linha.contains("sub")) {
                 System.out.println(teste.reduce(op_sub, 0));
+            } else if (linha.contains("map")) {
+                teste.map(x -> x * x);
             }
         }
     }
